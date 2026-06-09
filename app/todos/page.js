424f4b1383e9ee8114815212b54b45cf6c490a39
Himanshu <165992144+himanshu10rawat@users.noreply.github.com>
2026-06-09 12:26:58 +0530

    parallel data fetching using promise.all()

--- app/todos/page.js
+++ app/todos/page.js
@@ -1,30 +1,34 @@
 // Api call in server component
 
-import SlowApi from "@/components/SlowApi";
-import TodosList from "@/components/TodosList";
-import { Suspense } from "react";
+async function fetchData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
 
-export default function Todos() {
+export default async function Todos() {
+  const urls = [
+    "https://jsonplaceholder.typicode.com/todos?_limit=5",
+    "https://procodrr.vercel.app/?sleep=2000",
+    "https://procodrr.vercel.app/?sleep=3000",
+  ];
+
+  const [todos, data1, data2] = await Promise.all(
+    urls.map((url) => fetchData(url)),
+  );
+
   return (
     <>
       <h1>Todos</h1>
-      <Suspense
-        fallback={
-          <div className="todos-container">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <li key={index} className="shimmer">
-                <div className="shimmer-checkbox"></div>
-                <div className="shimmer-text"></div>
-              </li>
-            ))}
-          </div>
-        }
-      >
-        <TodosList />
-      </Suspense>
-      <Suspense fallback={<h2>Slow Api Loading...</h2>}>
-        <SlowApi />
-      </Suspense>
+
+      {todos.map(({ id, title, completed }) => (
+        <div key={id} style={{ display: "flex", gap: "0.5rem" }}>
+          <input type="checkbox" checked={completed} readOnly />
+          <p>{title}</p>
+        </div>
+      ))}
+
+      <h2>{JSON.stringify(data1)}</h2>
+      <h2>{JSON.stringify(data2)}</h2>
     </>
   );
 }
